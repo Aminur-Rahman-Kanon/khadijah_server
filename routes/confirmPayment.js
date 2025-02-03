13d98ed0ad327f7ad2abb8bebc85c1cd9626a49c
Aminur-Rahman-Kanon <kanon754@gmail.com>
2025-02-03 04:32:12 +0000

    paymnent_gateway_3

--- routes/confirmPayment.js
+++ routes/confirmPayment.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { sendOrderConfirmationPromise } = require('../utilities/utilities');
 
-router.post('/', (req, res) => {
-    const data = req.body;
+router.post('/', async (req, res) => {
+    const { userInput } = req.body;    
     
-    if (!data) return res.status(404).json({ status: 'no data provided' });
+    if (!userInput) return res.status(404).json({ status: 'no data provided' });
 
     //implement whatsapp and email integration
-    return res.status(200).json({ status: 'success' });
-    
+    sendOrderConfirmationPromise(userInput).then(result => {
+        if (result.status === 'success'){
+            return res.status(200).json({ status: 'success' })
+        }
+        else {
+            return res.status(400).json({ status: result.status, message: result.message })
+        }
+    }).catch(err => res.status(400).json({ status: 'failed', message: err.message }));
 })
 
 module.exports = router;
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,3 +1,6 @@
+const nodeMailer = require('nodemailer')
+const https = require('https');
+
 function cronJob (){
     setInterval(() => {
         https.get('https://khadijah-server.onrender.com', (res) => {
@@ -6,6 +9,62 @@ function cronJob (){
     }, 840000);
 }
 
+function sendOrderConfirmationPromise (booking) {
+    return new Promise( async (resolve, reject) => {
+    
+        try {
+            const transporter = nodeMailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: 'khadijahwebservice@gmail.com',
+                  pass: 'bjdrbgzhgxrtlqfd'
+                }
+            });
+    
+            if (!transporter) return { status: 'failed', message: 'no transporter found' };
+            
+            const mailOptions = {
+                from: 'khadijahwebservice@gmail.com',
+                to: 'kanon754@gmail.com',
+                subject: 'A new booking received',
+                text: 'Here is the booking details',
+                html: `<div>
+                    <div>
+                        <h2 style="margin: 5px 0; color: black;">Service Details</h2>
+                        <p style="margin: 5px 0; color: black;">Payment: <span style="display:block; color:green;">Verified</span></p>
+                        <p style="margin: 5px 0; color: black;">Price: ${booking.price}</p>
+                        <p style="margin: 5px 0; color: black;">Date: ${booking.date}</p>
+                        <p style="margin: 5px 0; color: black;">Type: ${booking.service}</p>
+                        <p style="margin: 5px 0; color: black;">Duration: ${booking.duration}</p>
+                        <p style="margin: 5px 0; color: black;">Start Time: ${booking.beginTime}</p>
+                        <p style="margin: 5px 0; color: black;">End Time: ${booking.endTime}</p>
+                    </div>
+                    <div>
+                        <h2 style="margin: 5px 0">Client's Details</h2>
+                        <p style="margin: 5px 0; color: black;">Name: ${booking.details.name}</p>
+                        <p style="margin: 5px 0; color: black;">Email: ${booking.details.email}</p>
+                        <p style="margin: 5px 0; color: black;">Phone number: ${booking.details.phoneNumber}</p>
+                        <p style="margin: 5px 0; color: black;">Additional Notes: ${booking.details.notes}</p>
+                    </div>
+                </div>`
+            };
+            
+            await transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    reject({ status: 'failed', message: error.message });
+                } else {
+                    resolve({ status: 'success', message: info })
+                }
+            });
+            
+        } catch (error) {
+            reject({ status: 'failed', message: error.message })
+        }
+    })
+}
+
+
 module.exports = {
-    cronJob
+    cronJob,
+    sendOrderConfirmationPromise
 }
